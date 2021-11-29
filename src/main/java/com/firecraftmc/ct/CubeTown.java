a2d10569398fb79d72bb047a8de889e2353ca570
Firestar311 <slaterandrew0398@gmail.com>
2021-11-28 19:46:08 -0600

    Moved to Maven from Gradle

--- src/main/java/com/firecraftmc/ct/CubeTown.java
+++ src/main/java/com/firecraftmc/ct/CubeTown.java
@@ -1,9 +1,10 @@
 package com.firecraftmc.ct;
 
+import com.starmediadev.utils.Utils;
+
 public class CubeTown {
     public static final String TD = "DIRECT";
     public static void main(String[] args) {
-        
     }
     
     //Process what would be a visit before any actions are taken and then produce messages/preventions based on role priority
