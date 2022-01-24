2effe51a19256b8c2676d5a004b981a4fa8082db
Firestar311 <slaterandrew0398@gmail.com>
2022-01-23 20:47:41 -0600

    Added some more stuff

--- src/main/java/com/firecraftmc/ct/object/role/impl/Disguiser.java
+++ src/main/java/com/firecraftmc/ct/object/role/impl/Disguiser.java
@@ -1,9 +1,9 @@
 package com.firecraftmc.ct.object.role.impl;
 
-import com.firecraftmc.ct.enums.Alignment;
-import com.firecraftmc.ct.enums.RoleType;
+import com.firecraftmc.ct.enums.*;
 import com.firecraftmc.ct.object.game.Game;
 import com.firecraftmc.ct.object.game.Player;
+import com.firecraftmc.ct.object.game.Target;
 import com.firecraftmc.ct.object.role.MafiaRole;
 
 public class Disguiser extends MafiaRole {
@@ -13,5 +13,12 @@ public class Disguiser extends MafiaRole {
         addAttributes("The disguised Mafia member will appear to have the same role as the non-mafia member to the Investigator and Sheriff.", 
                 "Your disguised Mafia member will appear to be the other person to a Lookout.", 
                 "When disguised as a Town member, Mafia visits are disregarded by Spy.");
+    }
+    
+    @Override
+    public boolean isValidTarget(GameState state, DayPhase dayPhase, NightPhase nightPhase, Target target) {
+       boolean valid = state == GameState.NIGHT && target != null; 
+        
+        return valid;
     }
 }
